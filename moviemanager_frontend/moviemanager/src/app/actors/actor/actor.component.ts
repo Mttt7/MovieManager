de0b69e8a20ca528427b276ee70e793040cefdc7
Michal Tomaszewski <groobytt@gmail.com>
2023-11-17 08:45:25 +0100

    add add and remove movie from actor feature

--- moviemanager_frontend/moviemanager/src/app/actors/actor/actor.component.ts
+++ moviemanager_frontend/moviemanager/src/app/actors/actor/actor.component.ts
@@ -2,20 +2,29 @@ import { Component } from '@angular/core';
 import { Actor } from '../../models/actor.model';
 import { ActorService } from '../../services/actor.service';
 import { ActivatedRoute } from '@angular/router';
+import { Movie } from '../../models/movie.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MovieService } from '../../services/movie.service';
 
 @Component({
   selector: 'app-actor',
   templateUrl: './actor.component.html',
-  styleUrl: '../../person.scss'
+  styleUrls: ['../../person.scss', './actor.component.scss']
 })
 export class ActorComponent {
+
+
 
 
   id: number = 0
   actor: Actor = null
 
-  constructor(private actorService: ActorService,
-    private route: ActivatedRoute) { }
+  movies: Movie[]
+  selectedMovieId: number = -1
+
+
+  constructor(private actorService: ActorService, private movieService: MovieService,
+    private route: ActivatedRoute, private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id']
@@ -24,8 +33,44 @@ export class ActorComponent {
   loadActor() {
     this.actorService.getActorWithMovies(this.id).subscribe(data => {
       this.actor = data as Actor
+      this.loadAllMovies()
     })
   }
+  loadAllMovies() {
+    this.movieService.getAllMovies().subscribe(data => {
+      let movies = data as Movie[]
+      this.movies = movies.filter(movie => {
+        return !this.actor.movies.find(m => m.id === movie.id)
+      }) as Movie[]
+    })
+  }
+
+  addMovie() {
+    if (this.selectedMovieId === -1) return
+    this.actorService.assignMovieToActor(this.id, this.selectedMovieId).subscribe(data => {
+      this.loadActor()
+    })
+    this.selectedMovieId = -1
+  }
+
+  deleteMovie(movieId: number) {
+    this.actorService.removeMovieFromActor(this.id, movieId).subscribe(() => {
+      this.loadActor()
+    })
+
+    let snackBarRef = this._snackBar.open('Movie Deleted', 'Undo', {
+      duration: 3000
+    })
+    snackBarRef.onAction().subscribe(() => {
+      this.actorService.assignMovieToActor(this.id, movieId).subscribe(data => {
+        console.log(data)
+        this.loadActor()
+      })
+    });
+  }
+
+
+
 
   getAge() {
     if (this.actor && this.actor.birthDate) {
